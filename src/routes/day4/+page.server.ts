58226188170be4afcfc461488a8d1fae91c2aa60
Sai <sainathkm@gmail.com>
2022-12-04 10:52:50 +0100

    Day 4 Done!

--- src/routes/day4/+page.server.ts
+++ src/routes/day4/+page.server.ts
@@ -9,16 +9,38 @@ export const load: PageServerLoad = async () => {
 	//split the data into an array
 	const dataArray = data.split('\n');
 
-	console.log(dataArray);
-
 	// Declare an array to store the results
 	let part1Result = 0;
 	let part2Result = 0;
 
 	// Loop through the array for Part 1
 	for (let i = 0; i < dataArray.length; i++) {
-		// find the length of the each line
-		const lineLength = dataArray[i].length;
+		const sectionArray = dataArray[i].split(',');
+
+		//console.log(sectionArray);
+
+		const Elf1SectionArray = sectionArray[0].split('-');
+		const Elf2SectionArray = sectionArray[1].split('-');
+
+		// Check if the first elf's section is within the second elf's section
+		if (
+			(parseInt(Elf1SectionArray[0]) <= parseInt(Elf2SectionArray[0]) &&
+				parseInt(Elf1SectionArray[1]) >= parseInt(Elf2SectionArray[1])) ||
+			(parseInt(Elf1SectionArray[0]) >= parseInt(Elf2SectionArray[0]) &&
+				parseInt(Elf1SectionArray[1]) <= parseInt(Elf2SectionArray[1]))
+		) {
+			part1Result++;
+		}
+
+		// Check if the two sections overlap
+		if (
+			(parseInt(Elf1SectionArray[0]) <= parseInt(Elf2SectionArray[0]) &&
+				parseInt(Elf1SectionArray[1]) >= parseInt(Elf2SectionArray[0])) ||
+			(parseInt(Elf1SectionArray[0]) >= parseInt(Elf2SectionArray[0]) &&
+				parseInt(Elf1SectionArray[0]) <= parseInt(Elf2SectionArray[1]))
+		) {
+			part2Result++;
+		}
 	}
 
 	// Return the results
